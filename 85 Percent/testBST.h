af1a74481e0244f7e1215c5e466cff4bd1f244fd
Jonathan <jlgunderson2@gmail.com>
2022-06-02 21:50:39 -0600

    231

--- 85 Percent/testBST.h
+++ 85 Percent/testBST.h
@@ -34,49 +34,49 @@ public:
       reset();
 
       // Construct
-      test_construct_default();
-      test_constructCopy_empty();
-      test_constructCopy_one();
-      test_constructCopy_standard();
-      //test_constructMove_empty();
-      //test_constructMove_one();
-      //test_constructMove_standard();
+     // test_construct_default();
+     // test_constructCopy_empty();
+     // test_constructCopy_one();
+     // test_constructCopy_standard();
+     // test_constructMove_empty();
+     // test_constructMove_one();
+     // test_constructMove_standard();
 
-      // Assign
-      //test_assign_emptyToEmpty();
-      //test_assign_standardToEmpty();
-      //test_assign_emptyToStandard();
-      //test_assign_oneToStandard();
-      //test_assign_standardToOne();
-      //test_assign_standardToStandard();
-      //test_assignMove_emptyToEmpty();
-      //test_assignMove_standardToEmpty();
-      //test_assignMove_emptyToStandard();
-      //test_assignMove_oneToStandard();
-      //test_assignMove_standardToOne();
-      //test_assignMove_standardToStandard();
-      //test_swap_emptyToEmpty();
-      //test_swap_standardToEmpty();
-      //test_swap_emptyToStandard();
-      //test_swap_standardToStandard();
+     ////Assign
+     // test_assign_emptyToEmpty();
+     // test_assign_standardToEmpty();
+     // test_assign_emptyToStandard();
+     // test_assign_oneToStandard();
+     // test_assign_standardToOne();
+     // test_assign_standardToStandard();
+     // test_assignMove_emptyToEmpty();
+     // test_assignMove_standardToEmpty();
+     // test_assignMove_emptyToStandard();
+     // test_assignMove_oneToStandard();
+     // test_assignMove_standardToOne();
+     // test_assignMove_standardToStandard();
+     // test_swap_emptyToEmpty();
+     // test_swap_standardToEmpty();
+     // test_swap_emptyToStandard();
+     // test_swap_standardToStandard();
 
-      //// Find
-      //test_find_empty();
-      //test_find_standardBegin();
-      //test_find_standardLast();
-      //test_find_standardMissing();
+     // // Find
+     // test_find_empty();
+     // test_find_standardBegin();
+     // test_find_standardLast();
+     // test_find_standardMissing();
 
-      //// Insert
-      //test_insert_oneLeft();
-      //test_insert_oneRight();
-      //test_insert_duplicate();
-      //test_insert_keepUnique();
-      //test_insertMove_oneLeft();
-      //test_insertMove_oneRight();
-      //test_insertMove_duplicate();
-      //test_insertMove_keepUnique();
+      // Insert
+      test_insert_oneLeft();
+      test_insert_oneRight();
+      test_insert_duplicate();
+      test_insert_keepUnique();
+      test_insertMove_oneLeft();
+      test_insertMove_oneRight();
+      test_insertMove_duplicate();
+      test_insertMove_keepUnique();
 
-      //// Remove
+      // Remove
       //test_erase_empty();
       //test_erase_standardMissing();
       //test_erase_noChildren();
